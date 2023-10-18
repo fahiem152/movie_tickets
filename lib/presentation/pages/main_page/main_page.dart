f21f4f60c18a8c071c6648877fce8866cc96b6e6
fahiem152 <amfahiem010502@gmail.com>
2023-10-18 11:19:56 +0700

    feat: bottom nav bar

--- lib/presentation/pages/main_page/main_page.dart
+++ lib/presentation/pages/main_page/main_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_tickets/presentation/extensions/build_context_extension.dart';
+import 'package:movie_tickets/presentation/msic/methods.dart';
 import 'package:movie_tickets/presentation/providers/routes/router_provider.dart';
 import 'package:movie_tickets/presentation/providers/user_data/user_data_provider.dart';
+import 'package:movie_tickets/presentation/widgets/bottom_nav_bar.dart';
+import 'package:movie_tickets/presentation/widgets/bottom_nav_bar_item.dart';
 
 class MainPage extends ConsumerStatefulWidget {
   const MainPage({
@@ -14,6 +17,8 @@ class MainPage extends ConsumerStatefulWidget {
 }
 
 class _MainPageState extends ConsumerState<MainPage> {
+  PageController pageController = PageController();
+  int selectedPage = 0;
   @override
   Widget build(BuildContext context) {
     ref.listen(userDataProvider, (previous, next) {
@@ -24,30 +29,75 @@ class _MainPageState extends ConsumerState<MainPage> {
       }
     });
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Main"),
-          actions: const [],
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(ref.watch(userDataProvider).when(
-                    data: (data) => data.toString(),
-                    error: (error, stackTrace) => error.toString(),
-                    loading: () => "Loading..",
-                  )),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blueGrey,
+        body: Stack(
+      children: [
+        PageView(
+          controller: pageController,
+          onPageChanged: (value) => setState(() {
+            selectedPage = value;
+          }),
+          children: const [
+            Center(
+              child: Text(
+                "Movies Page",
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
                 ),
-                onPressed: () {
-                  ref.read(userDataProvider.notifier).logout();
-                },
-                child: const Text("Logout"),
               ),
-            ],
-          ),
-        ));
+            ),
+            Center(
+              child: Text(
+                "Tickets Page",
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Profile Page",
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+        BottomNavBar(
+          items: [
+            BottomNavBarItem(
+              index: 0,
+              isSelected: selectedPage == 0,
+              title: 'Home',
+              image: 'assets/movie.png',
+              selectedImage: 'assets/movie-selected.png',
+            ),
+            BottomNavBarItem(
+              index: 1,
+              isSelected: selectedPage == 1,
+              title: 'Ticket',
+              image: 'assets/ticket.png',
+              selectedImage: 'assets/ticket-selected.png',
+            ),
+            BottomNavBarItem(
+              index: 2,
+              isSelected: selectedPage == 2,
+              title: 'Profile',
+              image: 'assets/profile.png',
+              selectedImage: 'assets/profile-selected.png',
+            ),
+          ],
+          onTap: (index) {
+            selectedPage = index;
+            pageController.animateToPage(selectedPage,
+                duration: Duration(milliseconds: 200), curve: Curves.easeInOut);
+          },
+          selectedIndex: 0,
+        )
+      ],
+    ));
   }
 }
